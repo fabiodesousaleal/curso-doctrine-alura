f69b5d2081bc94bcb932a1a7f09ca7a60ee953cd
fabiodesousaleal <fsl2@msn.com>
2021-04-09 10:51:35 -0300

    Implementando relacionamentos ManyToOne e OneToMany entre as classes Telefone e Aluno -obs: sem o flush

--- src/Entity/Aluno.php
+++ src/Entity/Aluno.php
@@ -2,6 +2,9 @@
 
 namespace Alura\Doctrine\Entity;
 
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+
 /**
  * @Entity
  */
@@ -18,6 +21,15 @@ class Aluno
      * @Column(type="string")
      */
     private $nome;
+    /**
+     * @OneToMany(targetEntity="Telefone", mappedBy="aluno")
+     */
+    private $telefones;
+
+    public function __construct()
+    {
+        $this->telefones = new ArrayCollection();
+    }
 
     public function getId(): int
     {
@@ -35,4 +47,15 @@ class Aluno
         return $this;
     }
 
+    public function addTelefones(Telefone $telefone)
+    {
+        $this->telefones->add($telefone);
+        $telefone->setAluno($this);
+        return $this;
+    }
+
+    public function getTelefones(): Collection
+    {
+        return $this->telefones;
+    }
 }
