110b37c6b2434771d193e1f5b1710191acaadb44
fabiodesousaleal <fsl2@msn.com>
2021-04-12 08:02:27 -0300

    Vinculando um Aluno a um Curso e vice-versa

---
+++ commands/vincular-aluno-curso.php
@@ -0,0 +1,33 @@
+<?php
+
+use Alura\Doctrine\Entity\Aluno;
+use Alura\Doctrine\Entity\Curso;
+use Alura\Doctrine\Helper\EntityManagerFactory;
+
+require_once __DIR__.'/../vendor/autoload.php';
+$entityManagerFactory = new EntityManagerFactory();
+$entityManager = $entityManagerFactory->getEntityManager();
+
+$idAluno = $argv[1];
+$idCurso = $argv[2];
+/**
+ * @var Curso $curso
+ */
+$curso = $entityManager->find(Curso::class, $idCurso);
+/**
+ * @var Aluno $aluno
+ */
+$aluno = $entityManager->find(Aluno::class, $idAluno);
+
+$aluno->addCurso($curso);
+/*
+pode ser usado também o metodo da classe aluno exemplo:
+$curso->addAluno($aluno);
+*/
+
+$entityManager->flush();
+
+
+
+
+
